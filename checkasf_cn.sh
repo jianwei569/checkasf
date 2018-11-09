7ae335f7ba27544b8fd195a823883356e2a73dec
jianwei569 <jianwei569@outlook.com>
2018-11-09 21:23:08 +0800

    Add files via upload

---
+++ checkasf_cn.sh
@@ -0,0 +1,114 @@
+#!/usr/bin/env bash
+#判断系统
+Get_Dist_Name()
+{
+    if grep -Eqii "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+        DISTRO='CentOS'
+        PM='yum'
+    elif grep -Eqi "Red Hat Enterprise Linux Server" /etc/issue || grep -Eq "Red Hat Enterprise Linux Server" /etc/*-release; then
+        DISTRO='RHEL'
+        PM='yum'
+    elif grep -Eqi "Aliyun" /etc/issue || grep -Eq "Aliyun" /etc/*-release; then
+        DISTRO='Aliyun'
+        PM='yum'
+    elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
+        DISTRO='Fedora'
+        PM='yum'
+    elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+        DISTRO='Debian'
+        PM='apt'
+    elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+        DISTRO='Ubuntu'
+        PM='apt'
+    elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
+        DISTRO='Raspbian'
+        PM='apt'
+    else
+        DISTRO='unknow'
+    fi
+    #echo $DISTRO;
+}
+Get_Dist_Name
+
+install_expect(){
+	#$PM
+	yum install -y expect
+}
+
+install_screen(){
+	#$PM
+	yum install -y screen
+}
+
+#判断是否有安装expect或screen
+#check_expect=$($PM | grep "expect")
+check_expect=$(yum list installed | grep "expect")
+
+if [[ $check_expect =~ "expect" ]]
+then
+        echo "expect已安装，继续执行！"
+else
+        echo "检测到未安装expect，自动安装!"
+        install_expect
+fi
+
+#check_screen=$($PM | grep "screen")
+check_screen=$(yum list installed | grep "screen")
+
+if [[ $check_screen =~ "screen" ]]
+then
+        echo "screen已安装，继续执行!"
+else
+        echo "检测到未安装screen，自动安装!"
+        install_screen
+fi
+
+ASF="/opt/ASF/ArchiSteamFarm"
+#判断ASF是否存在
+echo "默认ASF路径为$ASF"
+while :
+do
+	if [ -f $ASF ]
+	then
+		echo "ASF文件存在，继续执行！"
+		break
+	else	
+		echo "无法检测到ASF！"
+		read -p "是否输入新的ASF路径？y/n" yn
+		case $yn in
+		[Yy]) read ASF
+		;;
+		[Nn]) exit 0
+		;;
+		*) exit 0
+		;;
+		esac
+	fi
+done
+#判断asf状态
+#screen -ls | grep "asf"
+runasf(){
+cmd=$"$ASF";
+screen_name="asf"
+screen -dmS $screen_name
+screen -x -S $screen_name -p 0 -X stuff "$cmd"
+screen -x -S $screen_name -p 0 -X stuff '\n'
+/usr/bin/expect <<EOF
+send "\01"
+send "d"
+expect eof
+EOF
+}
+
+check_asf=$(screen -ls | grep "asf")
+#echo $check_asf
+echo "command(bash) results are:$check_asf"
+if [[ $check_asf =~ "asf" ]]
+then
+        echo "ASF已经在运行了！"
+else
+        echo "ASF未在运行! 启动ASF!"
+        runasf
+fi
+
+
