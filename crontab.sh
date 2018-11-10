efd76e3170cbac77028b0d1a2e84a39841fc428e
jianwei569 <jianwei569@outlook.com>
2018-11-10 22:59:17 +0800

    Add files via upload

---
+++ crontab.sh
@@ -0,0 +1,23 @@
+runasf(){
+cmd=$"$ASF";
+screen_name="asf"
+screen -dmS $screen_name
+screen -x -S $screen_name -p 0 -X stuff "$cmd"
+screen -x -S $screen_name -p 0 -X stuff '\n'
+/usr/bin/expect <<EOF
+send "\01"
+send "d"
+expect eof
+EOF
+}
+
+check_asf=$(screen -ls | grep "asf")
+#echo $check_asf
+echo "command(bash) results are:$check_asf"
+if [[ $check_asf =~ "asf" ]]
+then
+        echo "ASF is running !"
+else
+        echo "running ASF!"
+        runasf
+fi
